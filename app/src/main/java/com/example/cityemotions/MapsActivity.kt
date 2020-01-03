e32bcad8587716da2340fd021ed677e34db017b1
Xenobyte42 <andmicha2011@bk.ru>
2020-01-03 17:20:50 +0300

    Implement Google OAuth

--- app/src/main/java/com/example/cityemotions/MapsActivity.kt
+++ app/src/main/java/com/example/cityemotions/MapsActivity.kt
@@ -1,11 +1,17 @@
 package com.example.cityemotions
 
-import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.StrictMode
+import android.util.Log
 import com.example.cityemotions.fragments.*
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.Task
 
 
 interface OnSelectProfile {
@@ -20,21 +26,37 @@ interface  OnEmotionsClicker {
     fun onEmotionsClicked()
 }
 
-interface OnLoginClicker {
-    fun onLoginClicked()
+interface OnSignInClicker {
+    fun onSignIn()
 }
 
 
 class MapsActivity : AppCompatActivity(), OnSelectProfile, OnMarkerClicker, OnEmotionsClicker,
-    OnLoginClicker{
+    OnSignInClicker {
+
+    private val RC_SIGN_IN = 24
+    private lateinit var client: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        client = GoogleSignIn.getClient(this, gso)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, LoginFragment())
                 .commit()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account != null) {
+            logIn()
         }
     }
 
@@ -59,10 +81,33 @@ class MapsActivity : AppCompatActivity(), OnSelectProfile, OnMarkerClicker, OnEm
             .commit()
     }
 
-    override fun onLoginClicked() {
+    override fun onSignIn() {
+        val intent = client.signInIntent
+        startActivityForResult(intent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignIn(task)
+        }
+    }
+
+    private fun handleSignIn(task: Task<GoogleSignInAccount>) {
+        try {
+            val account = task.getResult(ApiException::class.java)
+            if (account != null) {
+                logIn()
+            }
+        } catch (t: ApiException) {
+            Log.e("AUTH", null, t)
+        }
+    }
+
+    private fun logIn() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, MapScreenFragment())
-            .addToBackStack(null)
             .commit()
     }
 }
